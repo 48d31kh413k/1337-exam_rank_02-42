1f36748a2394dbd52a11510e00d6b1aa226b6903
48d31kh413k <65833007+48d31kh413k@users.noreply.github.com>
2022-12-11 15:10:26 +0100

    Add files via upload

---
+++ Level_03/tab_mult/tab_mult.c
@@ -0,0 +1,58 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + 48);
+}
+
+int		ft_atoi(char *str)
+{
+	int i;
+	int result;
+
+	i = 0;
+	result = 0;
+	while (str[i] != '\0')
+	{
+		result *= 10;
+		result += str[i] - 48;
+		i++;
+	}
+	return (result);
+}
+
+int		main(int ac, char **av)
+{
+	int i;
+	int nb;
+
+	i = 1;
+	if (ac == 2)
+	{
+		nb = ft_atoi(av[1]);
+		while (i <= 9)
+		{
+			ft_putnbr(i);
+			write(1, " x ", 3);
+			ft_putnbr(nb);
+			write(1, " = ", 3);
+			ft_putnbr(nb * i);
+			if (i < 9)
+				write(1, "\n", 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
+}
