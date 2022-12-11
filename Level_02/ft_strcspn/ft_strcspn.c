c96dad8e7284e95bfe897cfd5831e330f04cc4df
48d31kh413k <65833007+48d31kh413k@users.noreply.github.com>
2022-12-11 15:08:39 +0100

    Add files via upload

---
+++ Level_02/ft_strcspn/ft_strcspn.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+size_t	ft_strcspn(const char *s, const char *reject)
+{
+	size_t i;
+	size_t i2;
+
+	i = 0;
+	i2 = 0;
+	while (s[i] != '\0')
+	{
+		i2 = 0;
+		while (reject[i2] != '\0')
+		{
+			if (s[i] == reject[i2])
+				return (i);
+			i2++;
+		}
+		i++;
+	}
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
